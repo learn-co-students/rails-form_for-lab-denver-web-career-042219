c5dd77679f7b01e173fa40ea43eb2ff71ee3468d
JosephPR <reilly4181@gmail.com>
2019-05-20 13:01:39 -0600

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+root 'students#index'
+resources :students do
+  resources :school_classes
+end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
